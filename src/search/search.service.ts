d22d3c8020e87c3428617b4475d4f381b9d8f25d
Phat Nguyen Luu <yadsendew@protonmail.com>
2022-05-22 03:07:04 +0000

    chore: clean up search service

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -1,32 +1,15 @@
-import { Client } from '@elastic/elasticsearch';
 import { SearchTotalHits } from '@elastic/elasticsearch/lib/api/types';
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { SearchDto } from './dto';
 import SearchResultDto from './dto/search-result.dto';
 import { ElasticsearchService } from './elasticsearch.service';
 
 @Injectable()
 export class SearchService {
-  private esClient: Client
 
-  constructor(
-    private configService: ConfigService,
-    private esService: ElasticsearchService
-  ) {
-    const cloudId = this.configService.get<string>("ES_CLOUD_ID")
-    const apiKey = this.configService.get<string>("ES_API_KEY")
-
-    this.esClient = new Client({
-      cloud: { id: cloudId },
-      auth: { apiKey: apiKey }
-    })
-
-
-  }
+  constructor(private esService: ElasticsearchService) {}
 
   async search(searchDto: SearchDto): Promise<SearchResultDto>  {
-    // this.esClient.search
     const searchResp = await this.esService.search(searchDto)
 
     const total = (typeof searchResp.hits.total === "number") ? searchResp.hits.total : (searchResp.hits.total as SearchTotalHits).value
