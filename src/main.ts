5fb4edaf937162945de32aedae9ac582415e6461
Phat Nguyen Luu <yadsendew@protonmail.com>
2022-05-24 06:12:05 +0000

    fix: allow cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: '*'
+  })
   await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
